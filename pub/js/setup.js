f1b394bb933cc9c295fed07b4c530aed63a890ae
Krasimir Berov <k.berov@gmail.com>
2011-02-28 01:49:55 +0200

    Working on initial system setup. Added credits. updated jq and ui

---
+++ pub/js/setup.js
@@ -0,0 +1,49 @@
+/**
+ * This script is used in index.xhtml to check if the system is operational
+ *
+ */
+function scripts_are_executable(){
+  scripts = ['mydlje','cpanel', 'site'];
+  var errors = 0;
+  for (i in scripts){
+    
+  $.ajax({
+    url: scripts[i] + '/hi.html',
+    success: function(data,succsess_code,jqXHR) { 
+    $('#system_check_div div.oks').show('slow');
+    $('#system_check_div ul.oks').append(
+      '<li>'+ scripts[i] + '/hi.html : ' + succsess_code + ', '+data + '</li>'  
+      );
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $('#system_check_div div.noks').show('slow');
+      $('#system_check_div ul.noks').append(
+        '<li>'+ scripts[i] + '/hi.html : ' + textStatus +  ', '+ errorThrown +  '</li>'  
+      );
+      $('#system_check_div div.wrench').show('slow');
+      $('#system_check_div ul.wrench').append(
+        '<li>Change "'+ scripts[i] + '" permissions to 0755 (rwxr-xr-x)'+  ' and refresh this page to see the result.</li>'  
+      );
+      errors++;
+    }
+  });
+  }//end for (i in scripts){
+
+  if(errors==0){
+    $('#system_check_h2').prepend('<span class="ui-icon ui-icon-check" style="float: left; margin-right: .3em;"></span>');
+  }
+  else {
+    $('#system_check_h2').prepend('<span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>');
+  }
+}//end function scripts_are_executable()
+
+$(window).load(function(){
+  $.ajaxSetup({
+    async:false,
+    cache:false,
+    context: $('#setup').get(),
+  });
+  // Accordion
+  $('#setup').accordion({ header: 'h2',autoHeight: false });
+  scripts_are_executable()
+});

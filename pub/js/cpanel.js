3380c3d14b22e2b3ff7fbb744418c251ae1d437c
Krasimir Berov <k.berov@gmail.com>
2011-04-07 18:43:45 +0300

    enhanced enhance_form()

--- pub/js/cpanel.js
+++ pub/js/cpanel.js
@@ -1,10 +1,26 @@
+/**
+ * Adds jQ UI help icon and functionality to display .help elements
+ * for each form separately.
+ * Adds jQ UI styling to input and select fields.
+ * Adds jQ UI styling to buttons.
+ */
 function enhance_form() {
   $('form input,form select').each(function() {
     $(this).addClass('ui-state-default')
   });
-  $('.help_icon').click(function() {
-    $('.help').toggle(200)
+  $('form').each(function() {
+    var form_object = this;
+    help_icon = $('<span id="'+ form_object.id+
+      '_help" class="get_help"><span class="ui-icon '+
+      'ui-icon-help"></span></span>')
+        .addClass("ui-corner-all ui-state-active")
+          .css({display: 'inline-block', cursor: 'pointer'
+          }).appendTo('#'+ form_object.id+' legend');
+    help_icon.click(function() {
+      $('.help',form_object).toggle(200)
+    });  
   });
+  
 
   $('form input[type="text"]').addClass('ui-corner-all');
 

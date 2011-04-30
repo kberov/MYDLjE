332e23fa4b92ca10c437b18c4fe918ed50ff853b
Krasimir Berov <k.berov@gmail.com>
2011-04-30 22:48:16 +0300

    New stubs

---
+++ perl/lib/MYDLjE/ControlPanel/C/Site.pm
@@ -0,0 +1,15 @@
+package MYDLjE::ControlPanel::C::Site;
+use MYDLjE::Base 'MYDLjE::ControlPanel::C';
+use Mojo::ByteStream qw(b);
+
+
+sub settings {
+  my $c = shift;
+
+#$c->render();
+  return;
+}
+
+
+1;
+

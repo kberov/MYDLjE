1853ecde3c25650222df9964ed24a9d681ee8afb
Krasimir Berov <k.berov@gmail.com>
2011-05-13 03:45:50 +0300

    listing domains - draft

--- perl/lib/MYDLjE/ControlPanel/C/Site.pm
+++ perl/lib/MYDLjE/ControlPanel/C/Site.pm
@@ -2,6 +2,22 @@ package MYDLjE::ControlPanel::C::Site;
 use MYDLjE::Base 'MYDLjE::ControlPanel::C';
 use Mojo::ByteStream qw(b);
 
+sub domains {
+  my $c   = shift;
+  my $uid = $c->msession->user->id;
+  $c->stash(
+    domains => [
+      $c->dbix->query(
+        "SELECT * FROM my_domains WHERE (user_id = ? AND permissions LIKE 'drw%')"
+          . " OR (group_id IN (SELECT gid FROM my_users_groups WHERE uid=?) "
+          . " AND permissions LIKE '____rw%') ORDER BY domain",
+        $uid, $uid
+        )->hashes
+    ]
+  );
+
+  return;
+}
 
 sub settings {
   my $c = shift;

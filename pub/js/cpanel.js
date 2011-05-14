1f0c8688ee67214fc3f0860ade2a361069737ae1
Krasimir Berov <k.berov@gmail.com>
2011-05-15 00:39:25 +0300

    Enhancements and fixes becide domains.

--- pub/js/cpanel.js
+++ pub/js/cpanel.js
@@ -5,17 +5,16 @@
  * Adds jQ UI styling to buttons.
  */
 function enhance_form() {
-  $('form input,form select').each(function() {
+  $('form input, form select').each(function() {
     $(this).addClass('ui-state-default')
   });
   $('form').each(function() {
     var form_object = this;
     help_icon = $('<span id="'+ form_object.id+
-      '_help" class="help_icon"><span class="ui-icon '+
+      '_help" class="legend_icon ui-corner-all"><span class="ui-icon '+
       'ui-icon-help"></span></span>')
-        .addClass("ui-corner-all ui-state-active")
-          .css({display: 'inline-block', cursor: 'pointer'
-          }).appendTo('#'+ form_object.id+' legend');
+        .addClass("ui-state-active")
+        .appendTo('#' + form_object.id + ' legend');
     help_icon.click(function() {
       $('.help',form_object).toggle(200)
     });  
@@ -36,4 +35,29 @@ function enhance_form() {
       primary: 'ui-icon-check'
     }
   });
+} // end function enhance_form()
+
+function enhance_list_items() {
+  var list_item = 'ul.items li';
+  $(list_item).hover(
+    function () {                                
+      $(this).removeClass("ui-state-default");
+      $(this).addClass("ui-state-hover");
+    },
+    function () {
+      $(this).removeClass("ui-state-hover");
+      $(this).addClass("ui-state-default");
+    }
+  );
 }
+
+
+
+/**
+  * Onload!
+  * Functionality which will be executed on each page when loaded.
+  */
+$(window).load(function() {
+  enhance_form();
+  enhance_list_items();
+});

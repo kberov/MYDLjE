d4ccff945e78fc1e71f9197750aa13fa8adae4d4
Krasimir Berov <k.berov@gmail.com>
2011-09-05 16:08:33 +0300

    #13 initial commit for MYDLjE::Template::PageContent

---
+++ perl/lib/MYDLjE/Template/PageContent.pm
@@ -0,0 +1,37 @@
+package MYDLjE::Template::PageContent;
+use MYDLjE::Base 'MYDLjE::Template';
+use utf8;
+
+require Mojo::Util;
+
+sub render {
+  my $self = shift;
+  return
+      'Hello World from '
+    . __PACKAGE__
+    . ' Called with page "'
+    . $self->get('TITLE') . '"';
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+MYDLjE::Template::PageContent - A front-end Content renderer
+
+
+=head1 SEE ALSO
+
+L<MYDLjE::Template>, L<MYDLjE::PageTree>
+
+
+=head1 AUTHOR AND COPYRIGHT
+
+(c) 2011 Красимир Беров L<k.berov@gmail.com>
+
+This code is licensed under LGPLv3.
+

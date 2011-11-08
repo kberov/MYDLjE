ef00384d04acca26ce8c0aed682a13c2dc7cb2ad
Krasimir Berov <k.berov@gmail.com>
2011-11-08 04:30:51 +0200

    #15 listing users

--- perl/lib/MYDLjE/ControlPanel/C/Accounts.pm
+++ perl/lib/MYDLjE/ControlPanel/C/Accounts.pm
@@ -2,6 +2,34 @@ package MYDLjE::ControlPanel::C::Accounts;
 use Mojo::Base 'MYDLjE::ControlPanel::C';
 use Mojo::ByteStream qw(b);
 
+sub users {
+  my $c = shift;
+  $c->stash(form  => {@{$c->req->params->params}});
+  $c->stash(users => $c->get_users());
+  $c->stash(now   => time);
+  return;
+}
+
+#Generates and executes an SQL query for selecting suers from db.
+#The where qlause is generated from the form.
+#returns an array of MYDLjE::M::User objects.
+sub get_users {
+  my $c     = shift;
+  my $form  = $c->stash('form');
+  my $where = {};
+  if ($form->{field}) {
+    $where->{$form->{field}} = {-like => $form->{like}};
+  }
+  my $users = [];
+  my ($sql, @bind) =
+    $c->dbix->abstract->select(MYDLjE::M::User->TABLE, MYDLjE::M::User->COLUMNS,
+    $where);
+  $sql .= $c->sql_limit($form->{offset}, $form->{rows});
+  foreach my $user (@{$c->dbix->query($sql, @bind)->hashes}) {
+    push @$users, $user;
+  }
+  return $users;
+}
 
 sub settings {
   my $c = shift;

7459cdf05c0e6c6854c18cd3245ad37071b63e1f
Krasimir Berov <k.berov@gmail.com>
2011-12-07 01:31:14 +0200

    Refactoring to expose $c->validate_and_login() for use wherever you see appropriate.

--- perl/lib/MYDLjE/ControlPanel/C/Auth.pm
+++ perl/lib/MYDLjE/ControlPanel/C/Auth.pm
@@ -18,7 +18,12 @@ sub loginscreen {
     $c->render();
   }
   elsif ($c->req->method eq 'POST') {
-    if ($c->_validate_and_login()) {
+    if ($c->validate_and_login()) {
+
+      #TODO: add hook on_login to do stuff each time a user logs in
+      require MYDLjE::ControlPanel::C::Site;
+      $c->MYDLjE::ControlPanel::C::Site::domains();
+
       $c->redirect_to('/home');
       return 1;
     }
@@ -30,64 +35,6 @@ sub loginscreen {
   return;
 }
 
-
-sub _validate_and_login {
-  my $c      = shift;
-  my $params = $c->req->params->to_hash;
-  if (($params->{session_id} || '') ne $c->msession->id) {
-    $c->stash(validator_errors => {session_id_error => $c->l('session_id_error')});
-    return 0;
-  }
-  $params->{login_name} =~ s/[^\p{IsAlnum}]//gx;
-
-  #TODO: Implement authorisation and access lists
-  # See http://www.perl.com/pub/2008/02/13/elements-of-access-control.html
-  # User is logged in if all conditions below apply:
-  #1. A user with this login name exists.
-  #2. Password md5_sum matches,
-  #3. Is not disabled
-  #4. Is within allowed period of existence if there is such
-  #5. Some of his groups namespaces allows this
-  my $mojo_app = $c->app->env->{MOJO_APP};
-  my $time     = time;
-  my $and      = <<"AND";
-    EXISTS (
-        SELECT g.id FROM groups g WHERE g.namespaces LIKE '%$mojo_app%' AND
-        g.id IN( SELECT ug.group_id FROM user_group ug WHERE ug.user_id=id)
-        )
-AND
-
-  my $user = MYDLjE::M::User->select(
-    login_name => $params->{login_name},
-    -and =>
-      [\'disabled=0', \$and, \"((start=0 OR start<$time) AND (stop=0 OR stop>$time))",],
-  );
-
-  unless ($user->id) {
-    $c->app->log->error('No such user:' . $params->{login_name});
-    $c->stash(validator_errors =>
-        {login_name_error => $c->l('login_field_error', $c->l('login_name'))});
-    return 0;
-  }
-
-  my $login_password_md5 =
-    Mojo::Util::md5_sum(($params->{session_id} || '') . $user->login_password);
-  if ($login_password_md5 ne $params->{login_password_md5}) {
-    $c->stash(validator_errors =>
-        {login_password_error => $c->l('login_field_error', $c->l('login_password'))});
-    return 0;
-  }
-  else {
-    $c->msession->sessiondata({});    #empty
-    $c->msession->user($user);        #efficiently log in user
-
-    #TODO: add hook on_login to do stuff each time a user logs in
-    require MYDLjE::ControlPanel::C::Site;
-    $c->MYDLjE::ControlPanel::C::Site::domains();
-    return 1;
-  }
-  return 0;
-}
 
 sub isauthenticated {
   my $c = shift;
--- perl/lib/MYDLjE/Plugin/Helpers.pm
+++ perl/lib/MYDLjE/Plugin/Helpers.pm
@@ -101,9 +101,64 @@ sub register {
   );
   require Mojo::JSON;
   $app->helper(json => sub { Mojo::JSON->new; });
-
+  $app->helper(validate_and_login => \&_validate_and_login);
   return;
 }    #end register
+
+sub _validate_and_login {
+  my $c      = shift;
+  my $params = $c->req->params->to_hash;
+  $c->debug($c->dumper($params));
+  if (($params->{session_id} || '') ne $c->msession->id) {
+    $c->stash(validator_errors => {session_id_error => $c->l('session_id_error')});
+    return 0;
+  }
+  $params->{login_name} =~ s/[^\p{IsAlnum}]//gx;
+
+  #TODO: Implement authorisation and access lists
+  # See http://www.perl.com/pub/2008/02/13/elements-of-access-control.html
+  # User is logged in if all conditions below apply:
+  #1. A user with this login name exists.
+  #2. Password md5_sum matches,
+  #3. Is not disabled
+  #4. Is within allowed period of existence if there is such
+  #5. Some of his groups namespaces allows this
+  my $mojo_app = $c->app->env->{MOJO_APP};
+  my $time     = time;
+  my $and      = <<"AND";
+    EXISTS (
+        SELECT g.id FROM groups g WHERE g.namespaces LIKE '%$mojo_app%' AND
+        g.id IN( SELECT ug.group_id FROM user_group ug WHERE ug.user_id=id)
+        )
+AND
+
+  my $user = MYDLjE::M::User->select(
+    login_name => $params->{login_name},
+    -and =>
+      [\'disabled=0', \$and, \"((start=0 OR start<$time) AND (stop=0 OR stop>$time))",],
+  );
+
+  unless ($user->id) {
+    $c->app->log->error('No such user:' . $params->{login_name});
+    $c->stash(validator_errors =>
+        {login_name_error => $c->l('login_field_error', $c->l('login_name'))});
+    return 0;
+  }
+
+  my $login_password_md5 =
+    Mojo::Util::md5_sum(($params->{session_id} || '') . $user->login_password);
+  if ($login_password_md5 ne $params->{login_password_md5}) {
+    $c->stash(validator_errors =>
+        {login_password_error => $c->l('login_field_error', $c->l('login_password'))});
+    return 0;
+  }
+  else {
+    $c->msession->sessiondata({});    #empty
+    $c->msession->user($user);        #efficiently log in user
+    return 1;
+  }
+  return 0;
+}
 
 1;
 
@@ -139,6 +194,48 @@ Params: C<$ui_language>:
 =head2 json
 
 Returns a L<Mojo::JSON> instance.
+
+=head2 validate_and_login
+
+Validates parameters passed via a login form and logs in the user. 
+Returns 1 on success, 0 otherwise.
+
+The expected parameters passed to the form are:
+
+  login_name, login_password_md5, session_id
+
+B<login_name>
+
+This value is checked if exists in the table users.
+If this user is found we check if the user is from a group that is allowed to access the current application (a per-app. authrization only). The next check is if the user is disabled. 
+Last check is if the user has "expired". See comments on fields in table users.
+
+B<session_id>
+
+This is the current C<$c->session('id')> value - a randomly generated md5 sum.
+
+B<login_password_md5>
+
+This is an md5_sum of the following (as made by login_form using JavaScript):
+
+  var temp_login_password = hex_md5(login_name + login_password );
+  $('#login_password_md5').val(hex_md5(session_id + temp_login_password));
+
+Passwords are stored in database as md5_sum of the concatenated username and password.
+
+You can create a username pasword on the command line and put it in the database field login_password. 
+
+  perl -MMojo::Util -e'print Mojo::Util::md5_sum("username"."<!1password").$/'
+
+  --then
+  UPDATE users SET login_password='20ab28c74122b193ff8e7a497b3d7049'
+  WHERE login_name='username'; 
+
+MYDLjE does not check for passwords passed in plain text. 
+On every login the checked C<login_password_md5> is salted with different C<session_id>.
+
+
+
 
 
 =head1 SEE ALSO
